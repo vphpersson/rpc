434d6f27536b5d0297b5175c77666af094ad6824
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2019-12-28 16:03:53 +0100

    Added `UnidimensionalConformantArray`.

--- rpc/ndr.py
+++ rpc/ndr.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 from dataclasses import dataclass, field
 from abc import ABC, abstractmethod
-from typing import Optional, Union, Iterator, Final, ClassVar
-from struct import unpack as struct_unpack, pack as struct_pack
+from typing import Optional, Union, Iterator, Final, ClassVar, Tuple
+from struct import unpack as struct_unpack, pack as struct_pack, error as struct_error, \
+    unpack_from as struct_unpack_from
 from uuid import UUID
 
 from rpc.structures.presentation_syntax import PresentationSyntax
@@ -103,3 +104,27 @@ class NullPointer(Pointer):
     def __init__(self):
         self.representation = b''
         self.referent_id = 0
+
+
+@dataclass
+class UnidimensionalConformantArray(NDRType):
+    representation: Tuple[Union[NDRType, bytes], ...]
+
+    @classmethod
+    def from_bytes(cls, data: bytes, size_per_element: int = 1) -> UnidimensionalConformantArray:
+        try:
+            maximum_count: int = struct_unpack('<I', data[:4])[0]
+            return cls(
+                representation=tuple(
+                    bytes(struct_unpack_from(size_per_element * 'B', buffer=data[4:], offset=i*size_per_element))
+                    for i in range(maximum_count)
+                )
+            )
+        except struct_error as e:
+            raise ValueError from e
+
+    def __bytes__(self) -> bytes:
+        return b''.join([
+            struct_pack('<I', len(self.representation)),
+            b''.join(bytes(element) for element in self.representation)
+        ])
