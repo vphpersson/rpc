87014bf5a5630f807807570eb385cb813a539ecc
Viktor Persson <vph.persson@gmail.com>
2020-01-13 22:50:06 +0100

    Reworked client protocol base.

--- rpc/utils/client_protocol_message.py
+++ rpc/utils/client_protocol_message.py
@@ -2,19 +2,15 @@ from __future__ import annotations
 from dataclasses import dataclass
 from abc import ABC, abstractmethod
 from enum import IntEnum
-from typing import Dict, Type, ClassVar, Optional
+from typing import Type, ClassVar, Optional
 from contextlib import suppress
+
+from msdsalgs.win32_error import Win32Error, Win32ErrorCode
 
 from rpc.connection import Connection as RPCConnection
 from rpc.pdu_headers.base import MSRPCHeader
 from rpc.pdu_headers.request_header import RequestHeader
 from rpc.pdu_headers.response_header import ResponseHeader
-
-
-class NoMatchingErrorClassError(Exception):
-    def __init__(self, return_code: IntEnum):
-        super().__init__(f'The return code {return_code} does not map to any error class.')
-        self.return_code = return_code
 
 
 class ClientProtocolMessage(ABC):
@@ -36,25 +32,7 @@ class ClientProtocolRequestBase(ClientProtocolMessage, ABC):
 @dataclass
 class ClientProtocolResponseBase(ClientProtocolMessage, ABC):
     REQUEST_CLASS: ClassVar[Type[ClientProtocolRequestBase]] = NotImplemented
-    ERROR_CLASS: ClassVar[Type[ClientProtocolResponseError]] = NotImplemented
-    return_code: IntEnum
-
-
-class ClientProtocolResponseError(Exception, ABC):
-    RETURN_CODE: IntEnum = NotImplemented
-    DESCRIPTION: str = NotImplemented
-    RETURN_CODE_TO_ERROR_CLASS: Dict[IntEnum, Type[ClientProtocolResponseError]] = NotImplemented
-
-    def __init__(self, response: ClientProtocolResponseBase):
-        super().__init__(self.DESCRIPTION)
-        self.response: ClientProtocolResponseBase = response
-
-    @classmethod
-    def from_response(cls, response: ClientProtocolResponseBase) -> ClientProtocolResponseError:
-        try:
-            return cls.RETURN_CODE_TO_ERROR_CLASS[response.return_code](response=response)
-        except KeyError as e:
-            raise NoMatchingErrorClassError(return_code=response.return_code) from e
+    return_code: Win32ErrorCode
 
 
 async def obtain_response(
@@ -89,10 +67,13 @@ async def obtain_response(
         # TODO: Use proper exception
         raise ValueError
 
-    response_error: Optional[ClientProtocolResponseError] = None
+    response_error: Optional[Win32Error] = None
     # Only return codes indicating errors map to an error class. Return codes for successes result in a lookup error.
-    with suppress(NoMatchingErrorClassError):
-        response_error = client_protocol_response.ERROR_CLASS.from_response(response=client_protocol_response)
+    with suppress(KeyError):
+        response_error = Win32Error.from_win32_error_code(
+            win32_error_code=client_protocol_response.return_code,
+            response=client_protocol_response
+        )
 
     if raise_exception and response_error is not None:
         raise response_error
--- setup.py
+++ setup.py
@@ -3,4 +3,7 @@ setup(
     name='rpc',
     version='0.1',
     packages=find_packages(),
+    install_requires=[
+        'msdsalgs @ https://github.com/vphpersson/msdsalgs/tarball/master'
+    ]
 )
