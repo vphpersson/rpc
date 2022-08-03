140bcc4af50654b18707ab20a93125ee642156af
Viktor Persson <vph.persson@gmail.com>
2022-08-04 01:19:35 +0200

    Add missing `types.py` file.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='rpc',
-    version='0.11',
+    version='0.12',
     packages=find_packages(),
     install_requires=[
         'msdsalgs @ git+https://github.com/vphpersson/msdsalgs.git#egg=msdsalgs',
