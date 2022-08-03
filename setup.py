a87f361c7fa16742945bf0a8a31f15f239f40436
Viktor Persson <vph.persson@gmail.com>
2022-08-04 00:54:00 +0200

    Use `https` instead of `ssh`.

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 setup(
     name='rpc',
-    version='0.1',
+    version='0.11',
     packages=find_packages(),
     install_requires=[
-        'msdsalgs @ git+ssh://git@github.com/vphpersson/msdsalgs.git#egg=msdsalgs',
-        'ndr @ git+ssh://git@github.com/vphpersson/ndr.git#egg=ndr'
+        'msdsalgs @ git+https://github.com/vphpersson/msdsalgs.git#egg=msdsalgs',
+        'ndr @ git+https://github.com/vphpersson/ndr.git#egg=ndr'
     ]
 )
